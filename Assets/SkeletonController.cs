f62100d2b0f07a97308ee145ba95d1c30c72f7de
Robin <Robin.kok@hva.nl>
2015-03-21 13:30:35 +0100

    Skeleton Idle Walking en Attack Animation. The Attack might be weird tho

---
+++ Assets/SkeletonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeletonController : MonoBehaviour {
+
+    public Animator SkeletonAnim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
